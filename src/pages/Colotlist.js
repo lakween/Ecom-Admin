55b4eb0b487e1c3933809b2dbf22320bf801a522
lakween <lakweensenathilake@gmail.com>
2023-08-19 21:51:34 +0530

    add color

--- src/pages/Colotlist.js
+++ src/pages/Colotlist.js
@@ -1,42 +1,59 @@
-import React from "react";
-import { Table } from "antd";
-
-const columns = [
-  {
-    title: "SNo",
-    dataIndex: "key",
-  },
-  {
-    title: "Name",
-    dataIndex: "name",
-  },
-  {
-    title: "Action",
-    dataIndex: "action",
-  },
-];
-
-const data1 = [];
-const generateData = (i) => ({
-  key: i,
-  name: 'John Blame ${i}',
-  product: 32,
-  status: 'London, Park Lane No. ${i}',
-});
-
-for (let i = 0; i < 46; i++) {
-  data1.push(generateData(i));
-} 
+import React, {useEffect, useState} from "react";
+import {Button, Table} from "antd";
+import {deleteDocument, getAllDocFromCollectionRT} from "../actions/CommonAction";
+import {toast} from "react-toastify";
 
 const Colorlist = () => {
-  return (
-    <div>
-      <h3 className="mb-4 title">Colors</h3>
-      <div>
-        <Table columns={columns} dataSource={data1} />
-      </div>
-    </div>
-  );
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        getAllDocFromCollectionRT('color', setData)
+    }, []);
+
+    const columns = [
+        {
+            title: "SNo",
+            dataIndex: "id",
+        },
+        {
+            title: "Color",
+            dataIndex: "color",
+            render: (color) => (
+                <div style={{backgroundColor:color,height:'20px',width:'20px',borderRadius:'100%'}}>
+                </div>
+            )
+        },
+        {
+            title: "Action",
+            dataIndex: "id",
+            render: (id) => (
+                <div>
+                    <Button onClick={()=>(onDeleteHandler(id))} className={'me-2'}>Delete</Button>
+                </div>
+
+            ),
+        },
+    ];
+
+    const onDeleteHandler = (id) => {
+        deleteDocument('color', id).then(() => {
+            toast.success('Color successfully deleted', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+        }).catch(() => {
+            toast.error('Failed to delete color.', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+        })
+    }
+    return (
+        <div>
+            <h3 className="mb-4 title">Colors</h3>
+            <div>
+                <Table columns={columns} dataSource={data}/>
+            </div>
+        </div>
+    );
 };
 
 export default Colorlist;
