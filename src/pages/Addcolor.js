fdc96a5f95b53fe004c4616edaf08e4264fb0405
lakween <lakweensenathilake@gmail.com>
2023-08-19 21:35:51 +0530

    add color

--- src/pages/Addcolor.js
+++ src/pages/Addcolor.js
@@ -1,27 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 import CustomInput from "../Components/CustomInput";
+import {createDocOfCollection} from "../actions/CommonAction";
+import {toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
 
 const Addcolor = () => {
-  return (
-    <div>
-      <h3 className="mb-4 title">
-        Add Color
-      </h3>
-      <div>
-        <form action="">
-          <CustomInput
-            type="color"
-            label="Enter Product Color"
-            id="color"
-          />
-          <button
-            className="btn btn-success border-0 rounded-3 my-5"
-            type="submit">Add Color
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+    const [form, setForm] = useState({})
+    const navigate = useNavigate()
+    const valueChangeHandler = (event) => {
+        let {name, value} = event.target
+        setForm({...form, [name]: value})
+    }
+
+    const addColorHandler = () => {
+
+        createDocOfCollection('category', form).then(() => {
+            toast.success('Category successfully added', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+
+            navigate('list-color')
+
+        }).catch(() => {
+            toast.error('Failed to add Category', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+        })
+    }
+
+    return (
+        <div>
+            <h3 className="mb-4 title">
+                Add Color
+            </h3>
+            <div>
+                <form action="">
+                    <CustomInput
+                        onChng={valueChangeHandler}
+                        type="color"
+                        label="Enter Product Color"
+                        id="color"
+                    />
+                    <button
+                        onClick={addColorHandler}
+                        className="btn btn-success border-0 rounded-3 my-5"
+                        type="submit">Add Color
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Addcolor;
