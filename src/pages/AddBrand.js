a9c6d8361b803bb75c805d101d6d901a080481d5
lakween <lakweensenathilake@gmail.com>
2023-08-20 12:11:45 +0530

    product tags

---
+++ src/pages/AddBrand.js
@@ -0,0 +1,51 @@
+import CustomInput from "../Components/CustomInput";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {createDocOfCollection} from "../actions/CommonAction";
+import {toast} from "react-toastify";
+
+const AddBrand = () => {
+    const [form, setForm] = useState({})
+    const navigate = useNavigate()
+
+    const valueChangeHandler = (event) => {
+        let {name, value} = event.target
+        setForm({...form, [name]: value})
+    }
+
+    const addTagHandler = () => {
+        createDocOfCollection('category', form).then(() => {
+            toast.success('Category successfully added', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+            navigate('/admin/list-category')
+        }).catch(() => {
+            toast.error('Failed to add Category', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+        })
+    }
+
+    return (
+        <div>
+            <h3 className="mb-4  title">
+                Add Tags
+            </h3>
+            <div>
+                <CustomInput
+                    onChng={valueChangeHandler}
+                    type="text"
+                    name="name"
+                    label="Enter Tag"
+                    id="blogcat"
+                />
+                <button onClick={addTagHandler}
+                        className="btn btn-success border-0 rounded-3 my-5"
+                >Add Tag
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default AddBrand
---
+++ src/pages/BrandList.js
@@ -0,0 +1,79 @@
+import {Button, Table} from "antd";
+import React, {useEffect, useState} from "react";
+import {deleteDocument, getAllDocFromCollectionRT} from "../actions/CommonAction";
+import {toast} from "react-toastify";
+import {Link} from "react-router-dom";
+
+const BrandList = ()=>{
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        getAllDocFromCollectionRT('product', setData)
+    }, []);
+
+    const onDeleteHandler = (id) => {
+        deleteDocument('product', id).then(() => {
+            toast.success('Product successfully deleted', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+        }).catch(() => {
+            toast.error('Failed to delete Product.', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+        })
+    }
+
+    const columns = [
+        {
+            title: "SNo",
+            dataIndex: "id",
+            render: (text) => (
+                <Link to={`/admin/product/${text}`}>{text}</Link>
+            ),
+        },
+        {
+            title: "Title",
+            dataIndex: "title",
+            sorter: (a, b) => a.title - b.title,
+        },
+        {
+            title: "Brand",
+            dataIndex: "brand",
+            sorter: (a, b) => a.brand - b.brand,
+        },
+        {
+            title: "Category",
+            dataIndex: "category",
+            sorter: (a, b) => a.category - b.category,
+        },
+        // {
+        //     title: "Color",
+        //     dataIndex: "color",
+        // },
+        {
+            title: "Price",
+            dataIndex: "price",
+            sorter: (a, b) => a.price - b.price,
+        },
+        {
+            title: "Action",
+            dataIndex: "id",
+            render: (text) => (
+                <div>
+                    <Button onClick={() => (onDeleteHandler(text))} className={'me-2'}>Delete</Button>
+                </div>
+            ),
+        },
+    ];
+
+    return (
+        <div>
+            <h3 className="mb-4 title">Products</h3>
+            <div>
+                <Table columns={columns} dataSource={data}/>
+            </div>
+        </div>
+    )
+}
+
+export default BrandList
