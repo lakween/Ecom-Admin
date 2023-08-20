ce8971bfd16c8cfa5b7180760d25fc2b7bcaf185
lakween <lakweensenathilake@gmail.com>
2023-08-20 20:08:25 +0530

    feat

--- src/pages/Categorylist.js
+++ src/pages/Categorylist.js
@@ -6,60 +6,60 @@ import {Link} from "react-router-dom";
 
 const Categorylist = () => {
 
-  const [data,setData] = useState()
+    const [data, setData] = useState()
 
 
-  useEffect(() => {
-    getAllDocFromCollectionRT('category',setData)
-  }, []);
+    useEffect(() => {
+        getAllDocFromCollectionRT('category', setData)
+    }, []);
 
-  const onDeleteHandler = (id)=>{
-    deleteDocument('category',id).then(()=>{
-      toast.success('category successfully deleted', {
-        position: toast.POSITION.BOTTOM_CENTER
-      });
-    }).catch(()=>{
-      toast.error('Failed to delete category.', {
-        position: toast.POSITION.BOTTOM_CENTER
-      });
-    })
-  }
+    const onDeleteHandler = (id) => {
+        deleteDocument('category', id).then(() => {
+            toast.success('category successfully deleted', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+        }).catch(() => {
+            toast.error('Failed to delete category.', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
+        })
+    }
 
-  const columns = [
-    {
-      title: "CNo",
-      dataIndex: "id",
-      sorter: (a, b) => a.id - b.id,
-      render: (text) => (
-          <Link to={`/admin/category/${text}`}>{text}</Link>
-      ),
-    },
-    {
-      title: "Title",
-      dataIndex: "name",
-      sorter: (a, b) => a.name - b.name,
-    },
+    const columns = [
+        {
+            title: "CNo",
+            dataIndex: "id",
+            sorter: (a, b) => a.id - b.id,
+            render: (text) => (
+                <Link to={`/admin/category/${text}`}>{text}</Link>
+            ),
+        },
+        {
+            title: "Title",
+            dataIndex: "name",
+            sorter: (a, b) => a.name - b.name,
+        },
 
-    {
-      title: "Action",
-      dataIndex: "id",
-      render: (text) => (
-          <div>
-            <Button onClick={()=>(onDeleteHandler(text))} className={'me-2'}>Delete</Button>
-          </div>
+        {
+            title: "Action",
+            dataIndex: "id",
+            render: (text) => (
+                <div>
+                    <Button onClick={() => (onDeleteHandler(text))} className={'me-2'}>Delete</Button>
+                </div>
 
-      ),
-    },
-  ];
+            ),
+        },
+    ];
 
-  return (
-    <div>
-      <h3 className="mb-4 title">Product Categories</h3>
-      <div>
-        <Table columns={columns} dataSource={data} />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h3 className="mb-4 title">Product Categories</h3>
+            <div>
+                <Table pagination={false} scroll={{x: 1500, y: 1000}} columns={columns} dataSource={data}/>
+            </div>
+        </div>
+    );
 };
 
 export default Categorylist;
