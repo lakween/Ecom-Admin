e7cd93ec2eea8aca3defe1b37454e7479719b163
lakween <lakweensenathilake@gmail.com>
2023-09-05 23:23:37 +0530

    feat

--- src/Components/MainLayout.js
+++ src/Components/MainLayout.js
@@ -5,15 +5,18 @@ import {
     NotificationOutlined,
     UserOutlined
 } from '@ant-design/icons';
-import { Button, Layout, Menu, theme } from 'antd';
+import { Button, Layout, Menu, theme, Avatar } from 'antd';
+import { IoIosNotifications } from "react-icons/io";
 import { AiOutlineDashboard, AiOutlineUser, AiOutlineShoppingCart, AiOutlineTag, AiOutlineBgColors } from 'react-icons/ai'
 import { BiCategoryAlt } from 'react-icons/bi'
 import { FaClipboardList } from 'react-icons/fa'
 import { TbBrandZhihu } from 'react-icons/tb'
 import React, { useContext, useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
+import { signOut } from "../actions/CommonAction";
 import { StoreContext } from "../providers/ContextProvider";
+import logoNew from "../images/logoNew.png"
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -56,7 +59,10 @@ const MainLayout = () => {
     return (
         <Layout>
             <Sider style={{ backgroundColor: '#e4e4f5' }} trigger={null} collapsible collapsed={collapsed}>
-                <div className="demo-logo-vertical h-[60px]" />
+                <div className='flex justify-center items-center w-full'>
+                    <img  style={{ backgroundColor: "#e4e4f5", width: '150px', height: '100px' }} src={logoNew} />
+
+                </div>
                 <Menu
                     style={{ backgroundColor: '#e4e4f5' }}
                     mode="inline"
@@ -145,17 +151,75 @@ const MainLayout = () => {
                 />
             </Sider>
             <Layout>
-                <Header className=' border-l-4 border-sky-500' style={{ backgroundColor: '#e4e4f5' }} >
-                    <Button
-                        type="text"
-                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                        onClick={() => setCollapsed(!collapsed)}
-                        style={{
-                            fontSize: '16px',
-                            width: 64,
-                            height: 64,
-                        }}
-                    />
+                <Header className=' bg-slate-50 px-0'  >
+                    <div className="flex justify-between align-items-center">
+                        <Button
+                            type="text"
+                            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                            onClick={() => setCollapsed(!collapsed)}
+                            style={{
+                                fontSize: '16px',
+                                width: 64,
+                                height: 64,
+                            }}
+
+                        />
+                        {/* <div className="position-relative">
+                            <IoIosNotifications className="fs-4" />
+                            <span className="badge bg-warning rounded-circle p-1 position-absolute">
+                                3
+                            </span>
+                        </div> */}
+
+                        <div className="d-flex gap-3 align-items-center px-5 dropdown">
+                            <div>
+                                {
+                                    user?.photoURL ?
+                                        <Avatar className={'ms-4'} shape="square" src={user?.photoURL} size={35}
+                                            icon={<UserOutlined />} /> :
+                                        <Avatar className={'ms-4'} shape="square" size={35} icon={<UserOutlined />} />
+                                }
+
+                                {/*<img*/}
+                                {/*    width={32}*/}
+                                {/*    height={32}*/}
+                                {/*    src=""*/}
+                                {/*    alt=""*/}
+                                {/*/>*/}
+                            </div>
+                            <div
+                                role="button"
+                                id="dropdownMenuLink"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                <h5 className="mb-0">{user?.firstName || '' + user?.lastName || ''}</h5>
+                                <p className="mb-0">{user?.email || ''}</p>
+                            </div>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <li>
+                                    <Link
+                                        className="dropdown-item py-1 mb-1"
+                                        style={{ height: "auto", lineHeight: "20px" }}
+                                        to="/admin/profile"
+                                    >
+                                        View Profile
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        className="dropdown-item py-1 mb-1"
+                                        style={{ height: "auto", lineHeight: "20px" }}
+                                        onClick={() => {
+                                            signOut(navigate)
+                                        }}
+                                    >
+                                        Signout
+                                    </Link>
+                                </li>
+                            </div>
+                        </div>
+                    </div>
                 </Header>
                 <Content
                     style={{
